89673801b989838cc34e9a13982055b1a1526bd6
Wagner Almeida <wdmeida@gmail.com>
2017-07-13 16:28:23 -0300

    Remove frontend

--- config/express.js
+++ config/express.js
@@ -9,11 +9,6 @@ module.exports = function() {
 	//Configurações da aplicação.
 	app.set('port', process.env.PORT || 80);
 
-	//Template engine que será usado para gerar as views e onde ficarão armazenadas.
-	app.set('view engine', 'ejs');
-	app.set('views', './app/views');
-	app.use(express.static('./public'));
-
 	//Solicita ao bodyParser que realize o parse de json e requisições com o corpo x-ww-form-urlencoded.
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
