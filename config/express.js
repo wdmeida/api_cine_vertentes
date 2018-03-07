9a182c57a4d758c751e4ee1d24dd9be5998d5279
Wagner Almeida <wdmeida@gmail.com>
2018-03-07 19:44:09 -0300

    Add mocha, chai and update packages

--- config/express.js
+++ config/express.js
@@ -1,4 +1,4 @@
-//Importa os módulos que serão utilizados.
+// Importa os módulos que serão utilizados.
 const express = require('express');
 const load = require('express-load');
 const bodyParser = require('body-parser');
@@ -7,16 +7,16 @@ const allowCors = require('./cors');
 module.exports = function() {
 	const app = express();
 
-	//Configurações da aplicação.
+	// Configurações da aplicação.
 	app.set('port', process.env.PORT || 8080);
 
-	//Solicita ao bodyParser que realize o parse de json e requisições com o corpo x-www-form-urlencoded.
+	// Solicita ao bodyParser que realize o parse de json e requisições com o corpo x-www-form-urlencoded.
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(allowCors);
 	app.use(require('method-override')());
 
-	//Carrega todos os scripts das pastas app/models, app/controller e app/routes e adiciona na instância do Express.
+	// Carrega todos os scripts das pastas app/models, app/controller e app/routes e adiciona na instância do Express.
 	load('models', {cwd: 'app'})
 		.then('controllers')
 		.then('routes')
