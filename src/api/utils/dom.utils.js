5b38eced741b421beff3b77c38fbfb42e7d9f6cb
Wagner Almeida <wdmeida@gmail.com>
2018-03-08 15:39:52 -0300

    Refactoring utils

--- src/api/utils/dom.utils.js
+++ src/api/utils/dom.utils.js
@@ -1,7 +1,16 @@
-const { JSDOM } = require('jsdom');
+const jsdom = require('jsdom');
+
+const { JSDOM } = jsdom;
 
 export function getDomFromURL(url = '') {
+  if (url.length === 0) throw new Error('URL is required');
   return JSDOM.fromURL(url);
 }
 
-export function getTextContent() {}
+export function getTextContent(dom = {}, selector = '') {
+  if (dom.window === undefined) throw new Error('DOM must contains a window property');
+
+  if (selector.length === 0) throw new Error('Selector is required');
+
+  return dom.window.document.querySelector(selector).textContent;
+}
--- tests/api/utils/dom.utils.spec.js
+++ tests/api/utils/dom.utils.spec.js
@@ -6,40 +6,69 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
 
-chai.use(sinonChai);
-sinonStubPromise(sinon);
-
 import {
   getDomFromURL,
   getTextContent,
 } from '@utils/dom.utils';
 
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
 describe('dom.utils', () => {
-  it('should have getDomFromURL method exists', () => {
-    expect(getDomFromURL).to.exist;
+  describe('Smoke tests', () => {
+    it('should have getDomFromURL method exists', () => {
+      expect(getDomFromURL).to.exist;
+    });
+
+    it('should have getTextContent method exists', () => {
+      expect(getTextContent).to.exist;
+    });
   });
 
-  it('should have getTextContent method exists', () => {
-    expect(getTextContent).to.exist;
+  describe('getDomFromURL', () => {
+    let stubedJSDOM;
+
+    beforeEach(() => {
+      stubedJSDOM = sinon.stub(JSDOM, 'fromURL');
+    });
+
+    afterEach(() => {
+      stubedJSDOM.restore();
+    });
+
+    it('should return an exception when the URL is invalid', () => {
+      expect(getDomFromURL).to.throw('URL is required');
+    });
+
+    it('should call fromURL function', () => {
+      const dom = getDomFromURL('url');
+      expect(stubedJSDOM).to.have.been.calledOnce;
+    });
+
+    it('should call fromURL with the correct URL', () => {
+      const dom = getDomFromURL('http://www.cinegloria.com/programacao/');
+      expect(stubedJSDOM).to.have.been.calledWith('http://www.cinegloria.com/programacao/');
+
+      const dom2 = getDomFromURL('https://www.cineplaza.com.br/');
+      expect(stubedJSDOM).to.have.been.calledWith('https://www.cineplaza.com.br/');
+    });
   });
-});
 
-describe('getDomFromURL', () => {
-  let promise;
-  let stubedJDOM;
+  describe('getTextContent', () => {
+    const dom = new JSDOM('<p>Hello<img src="foo.jpg"></p>', { includeNodeLocations: true });
 
-  beforeEach(() => {
-    stubedJDOM = sinon.stub(JSDOM, 'fromURL');
-    promise = stubedJDOM.returnsPromise();
-  });
+    it('should returns an exception with the dom is undefined', () => {
+      expect(() => getTextContent(undefined)).to.throw('DOM must contains a window property');
+    });
 
-  afterEach(() => {
-    stubedJDOM.restore();
-  });
+    it('should return an exception with the selector is empty', () => {
+      expect(() => getTextContent(dom)).to.throw('Selector is required');
+    });
 
-  it('should have return an exception if the url is not passed', () => {
-    const dom = getDomFromURL();
-    expect(dom).to.have.an({});
+    it('should call window with the correct value', () => {
+      const textContent = getTextContent(dom, 'p');
+      expect(textContent).to.have.equal('Hello');
+    });
   });
 });
 
