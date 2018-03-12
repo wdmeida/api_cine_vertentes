3d8de769798721a8767090d56c8eb7ea5bc63907
Wagner Almeida <wdmeida@gmail.com>
2018-03-12 18:48:44 -0300

    Config NODE_ENV

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const app = require('./config/express.config')();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+const start = () => app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
 
-module.exports = app; // for tests
+const stop = server => server.close();
+
+if (process.env.NODE_ENV !== 'test') {
+  const server = start();
+}
+
+module.exports = { start, stop };
