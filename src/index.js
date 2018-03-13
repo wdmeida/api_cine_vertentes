08ede76e3dd2f3ca5b9a0f806328faf70dcb4a19
Wagner Almeida <wdmeida@gmail.com>
2018-03-13 10:22:24 -0300

    Add integration test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 8080;
 
 const start = () => app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
 
-const stop = server => server.close();
+const stop = (server) => {
+  server.close();
+  console.log('Server is closed');
+};
 
 if (process.env.NODE_ENV !== 'test') {
   const server = start();
